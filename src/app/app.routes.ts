aef963d2be8419b64fd4df242067454d4ff25055
NikhilJosephSaji <32763132+NikhilJosephSaji@users.noreply.github.com>
2026-07-05 14:33:41 +0530

    Update app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
+import { adminGuard } from './admin/admin.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'admin/login',
+    loadComponent: () =>
+      import('./admin/admin-login/admin-login.component').then(m => m.AdminLoginComponent)
+  },
+  {
+    path: 'admin/dashboard',
+    loadComponent: () =>
+      import('./admin/admin-dashboard/admin-dashboard.component').then(m => m.AdminDashboardComponent),
+    canActivate: [adminGuard]
+  },
+  {
+    path: '**',
+    loadComponent: () =>
+      import('./app.component').then(m => m.AppComponent)
+  }
+];
