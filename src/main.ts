8efdd00772501212db51b4e46ee5b32797a5f27a
NikhilJosephSaji <32763132+NikhilJosephSaji@users.noreply.github.com>
2026-07-05 14:34:11 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
-import { AppComponent } from './app/app.component';
+import { AppShellComponent } from './app/app-shell.component';
 
-bootstrapApplication(AppComponent, appConfig)
+bootstrapApplication(AppShellComponent, appConfig)
   .catch((err) => console.error(err));
+
